7c6600a41b3b5c75080aede601e7a827c51a9cd0
Aleksandr Ivashchenko <aleksandr.ivashchenko@sigma.software>
2017-11-15 19:55:25 +0200

    Initial Commit

---
+++ MathPowerLogic/MathPowerLogic.h
@@ -0,0 +1,13 @@
+// 
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MathPowerLogic.
+FOUNDATION_EXPORT double MathPowerLogicVersionNumber;
+
+//! Project version string for MathPowerLogic.
+FOUNDATION_EXPORT const unsigned char MathPowerLogicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathPowerLogic/PublicHeader.h>
+
+
